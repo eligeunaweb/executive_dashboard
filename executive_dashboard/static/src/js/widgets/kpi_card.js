a3a5620332d845dd013b3b87288f2942848e0e6d
eligeunaweb <info@eligeunaweb.com>
2026-03-19 08:19:03 +0100

    Delete executive_dashboard directory

--- executive_dashboard/static/src/js/widgets/kpi_card.js
+++
@@ -1,8 +0,0 @@
-odoo.define("executive_dashboard.kpi_card", function(require) {
-"use strict";
-const { Component } = owl;
-class KPICard extends Component {}
-KPICard.template = "executive_dashboard.KPICard";
-KPICard.defaultProps = { color: "blue", icon: "📊", editMode: false };
-return { KPICard };
-});
